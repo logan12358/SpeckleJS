84a662051d9708bab0d006cea78caeb768a0a899
Logan Glasson <logan.glasson@aurecongroup.com>
2018-08-06 17:02:04 +1200

    Add very basic stream subscription

--- streams.js
+++ streams.js
@@ -1,3 +1,5 @@
+const { w3cwebsocket: WS } = require('websocket')
+
 let Streams = function(client) {
   this.client = client
 }
@@ -12,6 +14,12 @@ Streams.prototype.get = function(streamId) {
   return this.client.axios
   .get(`/streams/${streamId}`)
   .then(response => response.data.resource)
+}
+
+Streams.prototype.subscribe = function(streamId, messageHandler) {
+  ws: new WS(this.client.axios.defaults.baseURL.replace(/^http/, 'ws') + '/?access_token=undefined&stream_id=' + streamId + '&client_id=specklejs'),
+  ws.onopen = () => console.log("Websocket Opened!")
+  ws.onmessage = ({ data }) => messageHandler(JSON.parse(data))
 }
 
 module.exports = Streams
